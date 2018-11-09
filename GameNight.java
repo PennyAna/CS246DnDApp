6951c0721b2166ece0af694050876b8c22ee1dfe
Claire Haderlie <clairehaderlie@gmail.com>
2018-11-08 18:03:57 -0700

    Stubbed GameNight class as made by the brilliant Clawn.

---
+++ GameNight.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class GameNight {
+
+    //VARIABLES
+    private ArrayList<Table> tables;
+    private String houseRules;
+    private String date;
+
+
+    //FUNCTIONS
+    public void setTables(ArrayList<Table> newTables){ tables = newTables;}
+    public ArrayList<Table> getTables(){ return tables; }
+
+    public Table getTable(int index){ return tables.get(index); }
+
+    public void addTable(Table newTable){ tables.add(newTable); }
+
+    public String getHouseRules(){ return houseRules;}
+    public void setHouseRules(String rules){ houseRules = rules; }
+
+    public String getDate() { return date;}
+    public void setDate(String newDate) { date = newDate; }
+
+    public void replaceTable(int index, Table editedTable){
+        tables.set(index, editedTable);
+
+    }
+
+}
